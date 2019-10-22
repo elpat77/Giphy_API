5756a3d49d9b157b90bd591ec70ac404211e393e
elpat77 <aguilarpat7@gmail.com>
2019-10-22 14:55:16 -0700

    added functionality to display gifs depending on the dog breed button clicked

--- apps.js
+++ apps.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    let topics = ['German Shepherd', 'Labradoodle', 'Corgi', 'Australian Shepherd', 'Saint Bernard', 'Mountain Bernese'] ;
+    let topics = ['German Shepherd', 'Labradoodle', 'Corgi', 'Australian Shepherd', 'Saint Bernard', 'Akita'] ;
 // console.log (topics);
 
 //function for displayimng array data, starting empty 
@@ -24,34 +24,51 @@ $('button').on("click", function() {
     console.log("dog breed " + dogBreed);
     
     // let api_key = 'HfGRRO78dm2tvcRZ0xr3g7Z2KNg9cL1L&q';
-    const giphy_url = "https://api.giphy.com/v1/gifs/search?api_key=HfGRRO78dm2tvcRZ0xr3g7Z2KNg9cL1L&q=german%20shepherd&limit=25&offset=0&rating=PG-13&lang=en";
-    console.log ('giphyURL'+ giphy_url);
+    const giphy_url = "https://api.giphy.com/v1/gifs/search?api_key=HfGRRO78dm2tvcRZ0xr3g7Z2KNg9cL1L&q=" + dogBreed + "&limit=10&offset=0&rating=PG-13&lang=en";
+    console.log ('giphyURL '+ giphy_url);
     
+    // Performing an AJAX request with the queryURL
     $.ajax({
         method: 'GET',
         url: giphy_url
-    }).then(response => {
+    }).
+    // After data comes back from the request
+    then(response => {
+         console.log('response '+response);
+         
+    // storing the data from the AJAX request in the data variable
         const data = response.data;
-        console.log(data);
-        data.forEach(element => {
-            console.log(element.images.fixed_height.url);
+        console.log('data' + data);
+        
+        // Looping through each of the data items
+        for (let i = 0; i < data.length; i++) {
+            // Creating and storing a div tag
+            var breedDiv = $("<div>");
+            
+            // Creating and storing an image tag
+            var breedImage = $("<img>");
+            // Setting the src attribute of the image to a property pulled off the result item
+            breedImage.attr("src", data[i].images.original_still.url);
+            
+            // Appending thea image tag to the animalDiv
+            breedDiv.append(breedImage);
+            
+            // Prependng the animalDiv to the HTML page in the "#gif" div
+            $("#gif").prepend(breedDiv);
+            
+                    
+        // data.forEach(element => {
+        //     console.log(element.images.original_still.url);
+            
+        //     $('.gif').append(
+        //         `<img src="${element.images.original_still.url}">`
 
-            $('.gif_container').append(
-                `<img src="${element.images.fixed_height.url}">`
+        //     // $('.gif_container').append(
+        //     //     `<img src="${element.images.fixed_height.url}">`
 
-// $('#searchSubmit').on('click', function () {
-//     const searchText = $('#gifSearch').val();
-//     const queryUrl =
-//         'https://api.giphy.com/v1/gifs/search?api_key=' +
-//         api_key +
-//         '&q=' +
-//         searchText +
-//         '&limit=25&offset=0&rating=G&lang=en';
-//     console.log(queryUrl);
-
-
-)
-})
-})        
-})
-})
+        }
+ })
+ })        
+ })
+ 
+            
