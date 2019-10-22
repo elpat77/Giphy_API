1ae5774d76c81e65e179a16e1e5f8fa964925278
elpat77 <aguilarpat7@gmail.com>
2019-10-21 17:32:27 -0700

    adding onclick event

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    
+
     let topics = ['German Shepherd', 'Labradoodle', 'Corgi', 'Australian Shepherd', 'Saint Bernard', 'Mountain Bernese'] ;
-console.log (topics);
+// console.log (topics);
 
 //function for displayimng array data, starting empty 
 function displayButtons(){
@@ -10,15 +10,33 @@ function displayButtons(){
     for (let i = 0; i < topics.length; i++) {
         var b = $("<button>");
         b.addClass("breed");
-        b.attr("data-name", topics[i]);
+        b.attr("data-breed", topics[i]);
         b.text(topics[i]);
-        $("#buttons").append(b);
+        $('#buttons').append(b);
     }
 }
 
 displayButtons();
 
-// let api_key = 'z9HhR5kUuAW21CZS2yQ2XSzwkJtOwf5o&q';
+
+$('button').on("click", function() {
+    var dogBreed = $(this).attr("data-breed");
+    console.log("dog breed " + dogBreed);
+    
+    // let api_key = 'HfGRRO78dm2tvcRZ0xr3g7Z2KNg9cL1L&q';
+    const giphy_url = "https://api.giphy.com/v1/gifs/search?api_key=HfGRRO78dm2tvcRZ0xr3g7Z2KNg9cL1L&q=german%20shepherd&limit=25&offset=0&rating=PG-13&lang=en";
+    console.log ('giphyURL'+ giphy_url);
+    
+    $.ajax({
+        url: giphy_url,
+        method: "GET"
+      })
+.then(function(data) {
+    console.log(data);
+    var results = response.data;
+    console.log(results);
+
+    // $('.container').html(data.joke);
 
 
 // $('#searchSubmit').on('click', function () {
@@ -37,20 +55,11 @@ displayButtons();
 //     }).then(response => {
 //         const data = response.data;
 //         console.log(data);
-
-// Creating array of dog breeds
-
-
-
 //         // data.forEach(element => {
 //         //     console.log(element.images.fixed_height.url);
 
 //         //     $('.gifContainer').append(
 //         //         `<img src="${element.images.fixed_height.url}">`
-//         //     );
-//         // });
-//     // });
-
-// });
-// });}
+})
+})
 })
